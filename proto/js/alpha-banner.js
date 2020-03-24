ccbcd5d80eebfbd2eaf3ea40d61d3d8369846668
jmealing <31008799+jmealing@users.noreply.github.com>
2020-03-24 11:26:58 -0400

    add js for alpha banner

---
+++ proto/js/alpha-banner.js
@@ -0,0 +1,5 @@
+console.log("banner-start");
+
+	var alphaBanner = document.getElementsByTagName('BODY')[0];
+	alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top"><h2 class="wb-inv">Alpha</h2><div class="container"><small><label class="alpha-label">Alpha</label>&nbsp;&nbsp; This is an experimental version of Canada.ca for public testing.</small></div></section >');
+console.log("banner-end");
