5564a2998a44a01466ce8d23c2da1907c783a7bd
jmealing <31008799+jmealing@users.noreply.github.com>
2020-03-26 14:32:47 -0400

    Update contact-link-swap.js

--- proto/js/contact-link-swap.js
+++ proto/js/contact-link-swap.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
 	var linkSwap = document.querySelector('a[href="https://www.canada.ca/en/contact.html"]');
 	if (linkSwap) {
-		linkSwap.setAttribute('href', 'contact-us.html')
+		linkSwap.setAttribute('href', 'https://test.canada.ca/covid-19-guidance/proto/contact-us.html')
 	}
 	console.log("run");
 
