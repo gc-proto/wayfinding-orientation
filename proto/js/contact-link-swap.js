24a6771ddf78d1fa62ff4161f2ce5a98fc2c2260
patlaj <patlaj@gmail.com>
2020-04-24 17:22:41 -0400

    changing back

--- proto/js/contact-link-swap.js
+++ proto/js/contact-link-swap.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
 	var linkSwap = document.querySelector('a[href="https://www.canada.ca/en/contact.html"]');
 	if (linkSwap) {
-		linkSwap.setAttribute('href', 'http://test.canada.ca/covid-19-guidance/proto/0_archive-of-prev-content/cerb23-03-2020/cerb-cra/contact.html')
+		linkSwap.setAttribute('href', 'https://test.canada.ca/covid-19-guidance/proto/contact-us.html')
 	}
 	console.log("run");
 
