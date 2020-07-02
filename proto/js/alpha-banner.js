15ac33fcd33008f52f748c4052b0c871481cf632
jmealing <31008799+jmealing@users.noreply.github.com>
2020-07-02 11:23:12 -0400

    lower case 'a'

--- proto/js/alpha-banner.js
+++ proto/js/alpha-banner.js
@@ -1,4 +1,4 @@
 // if you want the banner off, just comment out the file 
 
 var alphaBanner = document.getElementsByTagName('BODY')[0];
-alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top" role="banner"><h2 class="wb-inv">This webpage is in Alpha</h2><div class="container"><small><span class="alpha-label">Alpha</span>&nbsp;&nbsp; This is an experimental version of Canada.ca for public testing.</small></div></section >');
+alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top" role="banner"><h2 class="wb-inv">This webpage is in alpha</h2><div class="container"><small><span class="alpha-label">Alpha</span>&nbsp;&nbsp; This is an experimental version of Canada.ca for public testing.</small></div></section >');
