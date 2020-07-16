bdfc24b49cf6804e59d491f94f5551e35d745936
Michel Tremblay <Michelt.tremblay@cra-arc.gc.ca>
2020-07-16 12:06:17 -0400

    updated ids for duplication. introduced add employee class

--- proto/js/employee-wage-form-display.js
+++ proto/js/employee-wage-form-display.js
@@ -3,15 +3,15 @@ $("#py-schl").on("change", function (event) {
   var target = event.target;
   //user chose weekly pay schedule
   if (target.value == "1") {
-    $("#emp1").removeClass('hidden');
-    $("#emp2").addClass('hidden');
+    $("#wkly-emp-1").removeClass('hidden');
+    $("#bwkly-emp-1").addClass('hidden');
   //user chose bi-weekly schedule
   } else if(target.value == "2") {
-    $("#emp2").removeClass('hidden');
-    $("#emp1").addClass('hidden');
+    $("#bwkly-emp-1").removeClass('hidden');
+    $("#wkly-emp-1").addClass('hidden');
   //default index selected
   } else {
-    $("#emp1").addClass('hidden');
-    $("#emp2").addClass('hidden');
+    $("#wkly-emp-1").addClass('hidden');
+    $("#bwkly-emp-1").addClass('hidden');
   }
 });
