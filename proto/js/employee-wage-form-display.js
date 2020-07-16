4bf0757ea5996ea70d5233d359991c78026e2eca
Michel Tremblay <Michelt.tremblay@cra-arc.gc.ca>
2020-07-16 12:42:10 -0400

    adding some polish

--- proto/js/employee-wage-form-display.js
+++ proto/js/employee-wage-form-display.js
@@ -1,6 +1,8 @@
 //display specific employee work hours panel based on pay schedule
 $("#py-schl").on("change", function (event) {
   var target = event.target;
+  $("#employees").removeClass('hidden');
+  $("#sendTo").removeClass('hidden');
   //user chose weekly pay schedule
   if (target.value == "1") {
     $("#wkly-emp-1").removeClass('hidden');
@@ -11,7 +13,10 @@ $("#py-schl").on("change", function (event) {
     $("#wkly-emp-1").addClass('hidden');
   //default index selected
   } else {
+    $("#employees").addClass('hidden');
+    $("#sendTo").addClass('hidden');
     $("#wkly-emp-1").addClass('hidden');
     $("#bwkly-emp-1").addClass('hidden');
   }
+  target.parentElement.scrollIntoView(true);
 });
