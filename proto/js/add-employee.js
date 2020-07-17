f150726c315cb3d8aafaf95b83a2e0dd378e70ab
Michel Tremblay <Michelt.tremblay@cra-arc.gc.ca>
2020-07-17 09:30:26 -0400

    adding new employees

--- proto/js/add-employee.js
+++ proto/js/add-employee.js
@@ -1,35 +1,37 @@
 
-var Employee = (function () {
-  var weekly = 1;
-  var biWeekly = 2;
-  var weeklyPrefix = "wkly-";
-  var biWeeklyPrefix = "bwkly-";
-  var weeklyTemplateId = weeklyPrefix +"emp-1";
-  var biWeeklyTemplateId = biWeeklyPrefix+ "emp-1";
-  var armsLengthId = "arms";
-  var baselineId = "baseline"
+var EmployeeFormGenerator = (function () {
+  
 
   /**
    * Employee()
    * Constructor
    * @param {Int} paySchedule the numeric value of the chosen pay schedule
    */
-  function Employee(paySchedule) {
+  function EmployeeFormGenerator(paySchedule) {
     this.paySchedule = paySchedule;
     this.updateTemplateId();
     this.numberOfEmployees = 1;
+    this.weekly = 1;
+    this.biWeekly = 2;
+    this.weeklyPrefix = "wkly-";
+    this.biWeeklyPrefix = "bwkly-";
+    this.weeklyTemplateId = this.weeklyPrefix +"emp-1";
+    this.biWeeklyTemplateId = this.biWeeklyPrefix+ "emp-1";
+    this.armsLengthId = "arms";
+    this.baselineId = "baseline"
   }
 
   /**
    * 
    */
-  Employee.prototype.generateNewEmployeeForm = function() {
-    if (this.paySchedule == weekly) {
-      //update number of employee frames
-      this.numberOfEmployees++;
+  EmployeeFormGenerator.prototype.generateNewEmployeeForm = function() {
+    if (this.paySchedule == this.weekly) {
       //generate another weekly pay template
-      return this.newWeeklyTemplate();
-    } else if (this.paySchedule == biWeekly) {
+      var newTemplate =  this.newWeeklyTemplate();
+      this.numberOfEmployees++;
+      //update number of employee frames
+      return newTemplate;
+    } else if (this.paySchedule == this.biWeekly) {
       //generate another bi-weekly pay template
       var newTemplate = this.newBiWeeklyTemplate();
       //update number of employee frames
@@ -42,57 +44,100 @@ var Employee = (function () {
 
   /**
    * 
-   * @param {*} newPaySchedule 
+   * @param {Int} newPaySchedule 
    */
-  Employee.prototype.updatePaySchedule = function(newPaySchedule) {
+  EmployeeFormGenerator.prototype.updatePaySchedule = function(newPaySchedule) {
     this.paySchedule = newPaySchedule;
+    this.updateTemplateId();
   }
 
   /**
    * 
    */
-  Employee.prototype.newBiWeeklyTemplate = function() {
+  EmployeeFormGenerator.prototype.newBiWeeklyTemplate = function() {
     var frag = document.getElementById(this.templateId);
+    frag = $(frag).clone(true,true);
+    $(frag).attr("id", $(frag).attr("id").replace("-1", "-" + (this.numberOfEmployees + 1)))
+    this.updateArmsLengthElementId(frag);
+    this.updateBaselineWageElementId(frag);
+    this.setUpdatesAndUpdatedByData(frag);
+    return frag;
   }
   
   /**
    * 
    */
-  Employee.prototype.newWeeklyTemplate = function() {
+  EmployeeFormGenerator.prototype.newWeeklyTemplate = function() {
     var frag = document.getElementById(this.templateId);
-
+    //clone the document fragment, with all children, and all attributes
+    frag = $(frag).clone(true, true);
+    $(frag).attr("id", $(frag).attr("id").replace("-1", "-" + (this.numberOfEmployees + 1)))
+    this.updateArmsLengthElementId(frag);
+    this.updateBaselineWageElementId(frag);
+    this.setUpdatesAndUpdatedByData(frag);
+    this.updateInputIds(frag);
+    return frag;
   }
 
   /**
    * 
    */
-  Employee.prototype.updateTemplateId = function() {
-    if (this.paySchedule == weekly) {
-      this.templateId = weeklyTemplateId;
+  EmployeeFormGenerator.prototype.updateTemplateId = function() {
+    if (this.paySchedule == this.weekly) {
+      this.templateId = this.weeklyTemplateId;
     } else {
-      this.templateId = biWeeklyTemplateId;
+      this.templateId = this.biWeeklyTemplateId;
     }
   }
 
   /**
    * 
-   * @param {*} frag 
+   * @param {DOMDocumentFragment} frag 
    */
-  Employee.prototype.updateArmsLengthElementId = function(frag) {
-    var armsLengthElement = frag.getElementById(
-      this.templateId + this.armsLengthId + this.numberOfEmployees
+  EmployeeFormGenerator.prototype.updateArmsLengthElementId = function(frag) {
+    var armsLengthElement = $(frag).find(
+      "#" + this.templateId + this.armsLengthId + this.numberOfEmployees
     );
-    armsLengthElement.id = this.templateId + this.armsLengthElement + (this.numberOfEmployees + 1)
+    console.debug( "#" + this.templateId + this.armsLengthId + this.numberOfEmployees);
+    $(armsLengthElement).attr("id", $(armsLengthElement).attr("id").replace(/-1$/g, "-" + (this.numberOfEmployees + 1)));
   }
 
   /**
    * 
-   * @param {*} frag 
+   * @param {DOMDocumentFragment} frag 
    */
-  Employee.prototype.updateArmsLengthElementId = function(frag) {
-    var baselineWageElement = frag.getElementById(
-      this.templateId + this.baselineId + this.numberOfEmployees
+  EmployeeFormGenerator.prototype.updateBaselineWageElementId = function(frag) {
+    var baselineWageElement = $(frag).find(
+      "#" + this.templateId + this.baselineId + this.numberOfEmployees
     );
-    baselineWageElement.id = this.templateId + this.baselineId + (this.numberOfEmployees + 1)
+    $(baselineWageElement).attr("id", $(baselineWageElement).attr("id").replace(/-1$/g, "-" + (this.numberOfEmployees + 1)));
   }
-})
+
+  /**
+   * 
+   * @param {DOMDocumentFragment} frag 
+   */
+  EmployeeFormGenerator.prototype.setUpdatesAndUpdatedByData = function(frag) {
+    var updateElements = $(frag).find("*[data-updates]");
+    var updatedByElements = $(frag).find("*[data-updated-by]");
+    $(updateElements).each(function(i, element) {
+      $(element).data("updates").replace(/-1$/g, "-" + (this.numberOfEmployees + 1));
+    });
+    $(updatedByElements).each(function(i, element) {
+      $(element).data("updated-by").replace(/-1$/g, "-" + (this.numberOfEmployees + 1));
+    });
+  }
+
+  /**
+   * 
+   * @param {DOMDocumentFragment} frag 
+   */
+  EmployeeFormGenerator.prototype.updateInputIds = function(frag) {
+    var inputs = $(frag).find("input[id=$'-1']");
+    $(inputs).each(function(i, element) {
+      $(element).attr("id", $(element).attr("id").replace(/-1$/g, "-" + (this.numberOfEmployees + 1)));
+    });
+  }
+
+  return EmployeeFormGenerator;
+})();
